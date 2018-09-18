0b2c67466d2fd64b043032fb4de5fb58ac826685
nielsrhansen <niels.r.hansen@math.ku.dk>
2018-09-19 00:41:08 +0200

    Minor corrections

--- kernel.R
+++ kernel.R
@@ -5,9 +5,32 @@ kernDens <- function (x, h, m = 512) {
   y <- numeric(m) ## The evaluations, initialized as a vector of zeroes
   ## The actual computation is done using nested for-loops. The outer loop
   ## is over the grid points, and the inner loop is over the data points.
-  for (i in seq_along(xx))
-    for (j in seq_along(x))
+  for (i in seq_along(xx)) {
+    for (j in seq_along(x)) {
       y[i] <- y[i] + exp(- (xx[i] - x[j])^2 / (2 * h^2))  
+    }
+  }
+  y <- y / (sqrt(2 * pi) * h * length(x))
+  list(x = xx, y = y)
+}
+
+kernDens_prof <- function (x, h, m = 512) {
+  rg <- range(x)
+  ## xx is equivalent to grid points in 'density'
+  xx <- seq(rg[1] - 3 * h, rg[2] + 3 * h, length.out = m)
+  y <- numeric(m) ## The evaluations, initialized as a vector of zeroes
+  ## The actual computation is done using nested for-loops. The outer loop
+  ## is over the grid points, and the inner loop is over the data points.
+  for (i in seq_along(xx)) {
+    for (j in seq_along(x)) {
+      ## y[i] <- y[i] + exp(- (xx[i] - x[j])^2 / (2 * h^2))  
+      z <- xx[i] - x[j]
+      z <- z^2
+      z <- z / (2 * h^2)
+      z <- exp(-z)
+      y[i] <- y[i] + z
+    }
+  }
   y <- y / (sqrt(2 * pi) * h * length(x))
   list(x = xx, y = y)
 }
@@ -18,15 +41,15 @@ kernDens_vec <- function (x, h, m = 512) {
   y <- numeric(m) 
   ## The inner loop from 'kernDens' has been vectorized, and only the 
   ## outer loop over the grid points remains.
-  for (i in seq_along(xx))
-    y[i] <- sum(exp(-(xx[i] - x)^2 / (2 * h^2)) / (sqrt(2 * pi) * h *length(x)))
+  for (i in seq_along(xx)) 
+    y[i] <- sum(exp(-(xx[i] - x)^2 / (2 * h^2)) / (sqrt(2 * pi) * h * length(x)))
   list(x = xx, y = y)
 }
 
 kernDens_apply <- function (x, h, m = 512) {
   rg <- range(x)
   xx <- seq(rg[1] - 3 * h, rg[2] + 3 * h, length.out = m)
-  y <- sapply(xx, function(z) sum(exp(-(z - x)^2 / (2 * h^2)) / (sqrt(2 * pi) * h *length(x))))
+  y <- sapply(xx, function(z) sum(exp(-(z - x)^2 / (2 * h^2)) / (sqrt(2 * pi) * h * length(x))))
   list(x = xx, y = y)
 }
 
@@ -36,4 +59,28 @@ kernDens_outer <- function (x, h, m = 512) {
   y <- outer(xx, x, function(zz, z) exp(-(zz - z)^2 / (2 * h^2)))
   y <- rowMeans(y) / (sqrt(2 * pi) * h)
   list(x = xx, y = y)
+}
+
+kernbin <- function(x, lo, hi, m) {
+  w <- numeric(m)
+  delta <- (hi - lo) / (m - 1)
+  for(i in seq_along(x)) {
+    ii <- floor((x[i] - lo) / delta + 0.5) + 1
+    w[ii] <- w[ii] + 1
+  }
+  w / sum(w)
+}
+
+## This implementation assumes a symmetric kernel! 
+## It's possible to avoid the symmetry assumption, 
+## but it's a little more complicated.
+kernDens_bin <- function(x, h, m = 512) {
+  ## Silverman's rule if h is missing
+  rg <- range(x) + c(- 3* h, 3 * h)
+  xx <- seq(rg[1], rg[2], length.out = m)
+  weights <- kernbin(x, rg[1], rg[2], m)
+  kerneval <- exp(- (xx - xx[1])^2 / (2 * h^2)) / (sqrt(2 * pi) * h)
+  kerndif <- matrix(kerneval[toeplitz(1:m)], m, m)
+  y <- colSums(weights * kerndif)
+  list(x = xx, y = y, h = h)
 }
