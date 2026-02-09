3b97736f17d336fcf0654efd0f00a69ba8393c8c
Johan Larsson <johan@jolars.co>
2026-02-09 13:02:54 +0100

    Autoformat data scripts

--- data/vegetables.R
+++ data/vegetables.R
@@ -1,6 +1,12 @@
 library(tidyverse)
-vegetables <- readr::read_csv("vegetables.csv", col_types = cols(store = "c")) %>% 
+vegetables <- readr::read_csv(
+  "vegetables.csv",
+  col_types = cols(store = "c")
+) %>%
   as.data.frame()
 
-save(vegetables, file = "../CSwR_package/data/vegetables.RData", compress = "xz")
-
+save(
+  vegetables,
+  file = "../CSwR_package/data/vegetables.RData",
+  compress = "xz"
+)
