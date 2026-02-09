dd419b3d8c6b534364948d5285244f8fc009b601
Johan Larsson <johan@jolars.co>
2026-02-09 12:03:29 +0100

    Autoformat

--- build_html_book.sh
+++ build_html_book.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 
 bookdown::render_book(
-  output_format = 'bookdown::bs4_book', 
+  output_format = 'bookdown::bs4_book',
   encoding = 'UTF-8',
-  output_dir = "../CSwR_html", 
+  output_dir = "../CSwR_html",
   clean = TRUE
 )
