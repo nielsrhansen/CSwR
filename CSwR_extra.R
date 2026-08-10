bccc0f9a596c2741d2aaac028db58ed487147018
Johan Larsson <johan@jolars.co>
2026-08-10 11:05:29 +0200

    Add support for algorithms in PDF and HTML

--- CSwR_extra.R
+++ CSwR_extra.R
@@ -7,3 +7,22 @@ benchTab <- function(x, ...) {
 
 # Set default ggplot2 theme
 ggplot2::theme_set(ggplot2::theme_bw())
+
+# Knitr engine for pseudocode blocks.
+# Body is algorithmicx-flavoured LaTeX (\begin{algorithm} ... \begin{algorithmic} ...).
+# HTML output is rendered client-side by pseudocode.js;
+# LaTeX output uses the algorithm + algpseudocode packages.
+knitr::knit_engines$set(algorithm = function(options) {
+  code <- paste(options$code, collapse = "\n")
+  if (knitr::is_html_output()) {
+    paste0(
+      "```{=html}\n<pre class=\"pseudocode\">\n",
+      code,
+      "\n</pre>\n```"
+    )
+  } else if (knitr::is_latex_output()) {
+    paste0("```{=latex}\n", code, "\n```")
+  } else {
+    code
+  }
+})
