b4304baa08f620fd56b72aa331167e86d343fd98
Johan Larsson <johan@jolars.co>
2026-08-10 11:10:08 +0200

    Add theme for schematic plots

--- CSwR_extra.R
+++ CSwR_extra.R
@@ -8,6 +8,31 @@ benchTab <- function(x, ...) {
 # Set default ggplot2 theme
 ggplot2::theme_set(ggplot2::theme_bw())
 
+# Minimal theme for schematic/pedagogical plots: axis titles only,
+# no grid, no ticks, no panel border. Axis lines on by default.
+theme_schematic <- function(
+  base_size = 11,
+  base_family = "",
+  axis_lines = TRUE
+) {
+  th <- ggplot2::theme_void(base_size = base_size, base_family = base_family) +
+    ggplot2::theme(
+      axis.title.x = ggplot2::element_text(margin = ggplot2::margin(t = 4)),
+      axis.title.y = ggplot2::element_text(
+        angle = 90,
+        margin = ggplot2::margin(r = 4)
+      ),
+      plot.background = ggplot2::element_rect(fill = NA, color = NA)
+    )
+  if (axis_lines) {
+    th <- th +
+      ggplot2::theme(
+        axis.line = ggplot2::element_line(colour = "black", linewidth = 0.3)
+      )
+  }
+  th
+}
+
 # Knitr engine for pseudocode blocks.
 # Body is algorithmicx-flavoured LaTeX (\begin{algorithm} ... \begin{algorithmic} ...).
 # HTML output is rendered client-side by pseudocode.js;
